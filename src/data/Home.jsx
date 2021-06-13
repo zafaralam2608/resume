831dbe483b7c9048a40f5fd76b23848620dec0c5
Md Zafar Alam <malam@axway.com>
2021-06-13 12:38:17 +0530

    update resume link

--- src/data/Home.jsx
+++ src/data/Home.jsx
@@ -7,11 +7,11 @@ export const home =
                 code: "91",
                 number: "9650955625"
             },
-        link: "https://drive.google.com/file/d/1dLf-1Ov2e1vot9cdlJ8DKwK2RzpP7byZ/view?usp=sharing",
+        link: "https://drive.google.com/uc?export=download&id=1dLf-1Ov2e1vot9cdlJ8DKwK2RzpP7byZ",
         contentList:
             [
                 "I'm an experienced Software Engineer with a demonstrated history of working in the Information Technology and Services industry.",
-                "In my 4 years career, I have developed a good grasp on programming languages namely Java and JavaScript as well as tools like Maven, NodeJS, Install4j and Install Anywhere along with multiple SQL database systems.",
+                "In my 4 years career, I have developed a good grasp on programming languages namely Java and JavaScript as well as tools like Maven, Install4j and Install Anywhere along with multiple SQL database systems like Oracle, SQLServer and Mysql.",
                 "My major roles mostly include web application development using Spring, ReactJS, REST and ActiveMQ.",
                 "Other important skills include Git, Jenkins, JIRA, Crucible and Confluence."
             ]
