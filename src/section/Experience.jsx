cf04d9a101a4a7ebc4b10d0acadb3fa0d085223f
Md Zafar Alam <zafaralam2608@gmail.com>
2022-11-24 22:57:19 +0530

    experience details

--- src/section/Experience.jsx
+++ src/section/Experience.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import {
-  Box, Divider, Tab, Tabs, Typography,
+  Accordion, AccordionDetails, AccordionSummary, Box, CardContent, CardHeader, Divider, Grid, List,
+  ListItem, Tab, Tabs, Typography,
 } from '@mui/material';
-import experienceItems from '../data/Education';
+import { ExpandMore } from '@mui/icons-material';
+import experienceItems from '../data/Experience';
 
 export default function Experience() {
   const [value, setValue] = useState(0);
@@ -11,20 +13,58 @@ export default function Experience() {
     setValue(newValue);
   };
 
+  const current = experienceItems[value];
+
   return (
     <Box id="0">
       <Typography variant="h3">Experience</Typography>
       <Divider sx={{ width: '300px' }} />
-      <Tabs
-        orientation="vertical"
-        value={value}
-        onChange={handleChange}
-        sx={{ borderRight: 1, borderColor: 'divider' }}
-      >
-        {experienceItems.map((experience) => (
-          <Tab label={experience.org} />
-        ))}
-      </Tabs>
+      <Grid container>
+        <Grid>
+          <Tabs
+            orientation="vertical"
+            value={value}
+            onChange={handleChange}
+            sx={{ borderRight: 1, borderColor: 'divider' }}
+          >
+            {experienceItems.map((experience) => (
+              <Tab label={experience.org} />
+            ))}
+          </Tabs>
+        </Grid>
+        <Grid>
+          <CardHeader
+            title={current.post}
+            subheader={`${current.org}, ${current.location}`}
+          />
+          <CardContent>
+            {
+              current.projects.map((project) => (
+                <Accordion>
+                  <AccordionSummary expandIcon={<ExpandMore />}>
+                    <CardHeader
+                      title={project.title}
+                      subheader={project.technologies.join(' | ')}
+                    />
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography paragraph>
+                      {project.description}
+                    </Typography>
+                    <List>
+                      {project.contributions.map((contribution) => (
+                        <ListItem>
+                          <Typography>{`* ${contribution}`}</Typography>
+                        </ListItem>
+                      ))}
+                    </List>
+                  </AccordionDetails>
+                </Accordion>
+              ))
+            }
+          </CardContent>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
